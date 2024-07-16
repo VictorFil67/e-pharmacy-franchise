b98a834098363d3682f0eda769bfc713092f81a8
VictorFil67 <victorfilinovych@gmail.com>
2024-07-16 21:17:17 +0300

    fixed export selectAllProducts

--- src/store/shops/selectors.js
+++ src/store/shops/selectors.js
@@ -1,3 +1,4 @@
 export const selectShop = (state) => state.shops.shop;
 export const selectShopProducts = (state) => state.shops.shopProducts;
+export const selectAllProducts = (state) => state.shops.allProducts;
 export const selectTotal = (state) => state.shops.total;
