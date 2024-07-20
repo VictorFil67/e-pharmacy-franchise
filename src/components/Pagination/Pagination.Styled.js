54f928d35c52e5e8329f6019dab3b69d07400851
VictorFil67 <victorfilinovych@gmail.com>
2024-07-21 01:42:36 +0300

    completed reviews tab

--- src/components/Pagination/Pagination.Styled.js
+++ src/components/Pagination/Pagination.Styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Edge = styled.button`
-  width: 35px;
-  height: 35px;
+  width: 32px;
+  height: 32px;
   border-radius: 50%;
   border: 1px solid rgba(29, 30, 33, 0.2);
   display: flex;
@@ -11,25 +11,46 @@ export const Edge = styled.button`
   background-color: var(--white);
   & > span {
     transform: scaleX(1.5);
-    font-size: 28px;
+    font-size: 24px;
+    font-weight: 300;
+  }
+`;
+export const Transit = styled.button`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid rgba(29, 30, 33, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--white);
+  & > span {
+    transform: scaleX(0.6);
+    font-size: 24px;
     font-weight: 300;
   }
 `;
 
 export const PaginationWrap = styled.ul`
   display: flex;
-  gap: 4px;
+  justify-content: space-between;
+  /* gap: 4px; */
+  width: 100%;
+  margin-top: 40px;
+  padding-bottom: 24px;
   & > .pagination {
     display: flex;
     align-items: center;
+    /* justify-content: space-between; */
     gap: 4px;
+    /* width: 100%; */
   }
   & .page,
-  .previous,
-  .next,
+  /* .previous, */
+  /* .next, */
   .break {
-    width: 35px;
-    height: 35px;
+    width: 32px;
+    height: 32px;
     border-radius: 50%;
     border: 1px solid rgba(29, 30, 33, 0.2);
     display: flex;
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,5 @@
 import ReactPaginate from "react-paginate";
-import { Edge, PaginationWrap } from "./Pagination.Styled";
+import { Edge, PaginationWrap, Transit } from "./Pagination.Styled";
 
 export const Pagination = ({
   currentPage,
@@ -11,24 +11,30 @@ export const Pagination = ({
     <nav>
       <PaginationWrap>
         {/* <li className={`page-item ${currentPage === 0 ? "disabled" : ""}`}> */}
-        <Edge>
-          <button
+        {/* <Edge> */}
+        {/* <button
             className="page-link"
             onClick={() => setCurrentPage(0)}
             disabled={currentPage === 0}
-          >
-            «
-          </button>
+          > */}
+        <Edge
+          //   className="page-link"
+          onClick={() => setCurrentPage(0)}
+          disabled={currentPage === 0}
+        >
+          <span>&#171;</span>
+          {/* « */}
         </Edge>
-        <li className={`page-item ${currentPage === 0 ? "disabled" : ""}`}>
-          <button
-            className="page-link"
-            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 0))}
-            disabled={currentPage === 0}
-          >
-            &lt;
-          </button>
-        </li>
+        {/* </Edge> */}
+        {/* <li className={`page-item ${currentPage === 0 ? "disabled" : ""}`}> */}
+        <Transit
+          // className="page-link"
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 0))}
+          disabled={currentPage === 0}
+        >
+          <span> &lt;</span>
+        </Transit>
+        {/* </li> */}
         <ReactPaginate
           previousLabel={""}
           nextLabel={""}
@@ -46,44 +52,45 @@ export const Pagination = ({
           pageClassName="page"
           //   pageLinkClassName={"page-link"}
           //   previousClassName={"page-item"}
-          previousClassName="previous"
+          //   previousClassName="previous"
           //   previousLinkClassName={"page-link"}
-          previousLinkClassName="previousLink"
+          //   previousLinkClassName="previousLink"
           //   nextClassName={"page-item"}
-          nextClassName="next"
+          //   nextClassName="next"
           //   nextLinkClassName={"page-link"}
-          nextLinkClassName="nextLink"
+          //   nextLinkClassName="nextLink"
           //   breakLinkClassName={"page-link"}
           disabledClassName={"disabled"}
+          forcePage={currentPage} //force sets current page
         />
-        <li
+        {/* <li
           className={`page-item ${
             currentPage === pageCount - 1 ? "disabled" : ""
           }`}
+        > */}
+        <Transit
+          //   className="page-link"
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, pageCount - 1))
+          }
+          disabled={currentPage === pageCount - 1}
         >
-          <button
-            className="page-link"
-            onClick={() =>
-              setCurrentPage((prev) => Math.min(prev + 1, pageCount - 1))
-            }
-            disabled={currentPage === pageCount - 1}
-          >
-            &gt;
-          </button>
-        </li>
-        <li
+          <span>&gt;</span>
+        </Transit>
+        {/* </li> */}
+        {/* <li
           className={`page-item ${
             currentPage === pageCount - 1 ? "disabled" : ""
           }`}
+        > */}
+        <Edge
+          // className="page-link"
+          onClick={() => setCurrentPage(pageCount - 1)}
+          disabled={currentPage === pageCount - 1}
         >
-          <button
-            className="page-link"
-            onClick={() => setCurrentPage(pageCount - 1)}
-            disabled={currentPage === pageCount - 1}
-          >
-            »
-          </button>
-        </li>
+          <span>»</span>
+        </Edge>
+        {/* </li> */}
       </PaginationWrap>
     </nav>
   );
