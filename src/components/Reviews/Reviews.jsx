92bed4df720f1a067e9aedb67d8b565172c92f8d
VictorFil67 <victorfilinovych@gmail.com>
2024-07-20 21:34:43 +0300

    created base pagination

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -7,18 +7,78 @@ import {
   Testimonial,
 } from "./Reviews.Styled";
 import { selectProduct } from "../../store/products/selectors";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
 
 export const Reviews = () => {
   const { reviews } = useSelector(selectProduct);
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const reviewsPerPage = 4;
+
+  const indexOfLastItem = (currentPage + 1) * reviewsPerPage;
+  const indexOfFirstItem = indexOfLastItem - reviewsPerPage;
+  const currentReviews = reviews.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
   return (
-    <ReviewsList>
-      {reviews.map((review) => (
-        <ItemWrap key={review._id}>
-          <Author>{review.name}</Author>
-          <Rating>Rating:{review.rating}</Rating>
-          <Testimonial>{review.testimonial}</Testimonial>
-        </ItemWrap>
-      ))}
-    </ReviewsList>
+    <div>
+      <ReviewsList>
+        {currentReviews.map((review) => (
+          <ItemWrap key={review._id}>
+            <Author>{review.name}</Author>
+            <Rating>Rating:{review.rating}</Rating>
+            <Testimonial>{review.testimonial}</Testimonial>
+          </ItemWrap>
+        ))}
+      </ReviewsList>
+      <ReactPaginate
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={Math.ceil(reviews.length / reviewsPerPage)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakLinkClassName={"page-link"}
+        disabledClassName={"disabled"}
+        renderFirstButton={() => (
+          <li className="page-item">
+            <a
+              onClick={() => setCurrentPage(0)}
+              href="!#"
+              className="page-link"
+            >
+              «
+            </a>
+          </li>
+        )}
+        renderLastButton={() => (
+          <li className="page-item">
+            <a
+              onClick={() =>
+                setCurrentPage(Math.ceil(reviews.length / reviewsPerPage) - 1)
+              }
+              href="!#"
+              className="page-link"
+            >
+              »
+            </a>
+          </li>
+        )}
+      />
+    </div>
   );
 };
